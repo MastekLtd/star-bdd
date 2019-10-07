c753cf087034e997b57f96e17798e3355ecca003
Ravi Salunkhe <ravi.salunkhe@mastek.com>
2019-10-07 14:11:38 +0530

    made changes to conftest.py

--- tests/step_defs/conftest.py
+++ tests/step_defs/conftest.py
@@ -4,12 +4,43 @@ from selenium import webdriver
 from core.commons import CommonFunctions
 
 
+# global feature_name = None
+
+
+# Before context
+def pytest_bdd_before_scenario(request, feature, scenario):
+    print('########## BEFORE SCENARIO ##############')
+    print(feature.name)
+
+
+def pytest_bdd_after_scenario(request, feature, scenario):
+    print('########## AFTER SCENARIO  ##############')
+
+
+def pytest_bdd_before_step_call(request, feature, scenario, step, step_func, step_func_args):
+    print('########## BEFORE STEP  ##############')
+
+
+def pytest_bdd_after_step(request, feature, scenario, step, step_func, step_func_args):
+    print('########## AFTER STEP  ##############')
+
+
+def pytest_bdd_step_error(request, feature, scenario, step, step_func, step_func_args, exception):
+    print('########## STEP ERROR  ##############')
+
+
+def pytest_bdd_step_validation_error(request, feature, scenario, step, step_func, step_func_args, exception):
+    print('########## VALIDATION ERROR  ##############')
+
+
 # Fixtures
 @pytest.fixture
 def driver(request):
     try:
         scenario_name = str(request.node.originalname).replace('test_', '')
         print(scenario_name)
+        # print('Feature name ==>> ' + feature.name)
+        print('Scenario Outline name ==>> ' + request.node.originalname)
         key_value = CommonFunctions.get_browser_mapping(scenario_name)
         print('key_value ======>>>>> ' + key_value)
 
